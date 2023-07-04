b5c9466b6efb1e25e20ddc903823615420f5cd57
minomad <93kymins@gmail.com>
2023-07-04 11:18:46 +0900

    js var const 학습

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,55 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let name;
+
+let admin;
+
+//참조에 의한 복사
+
+name = 'tiger';
+admin = name;
+
+name = 'beom'
+// console.log(admin);
+
+const ourPlanetName = 'earth';
+const currentOnlineUserName = 'tiger'
+
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기 
+let calcProductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hasClassName= true;
+
+
+// - 구매 결제 내역
+let paymentHistory;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+
+// - 오늘의 운세
+let fortuneOfToday;
+
+// - 상품 정보
+const productInfo = '';
+
+
+/* variables ----------------------------------------------------------- */
+
+
+/* constant variables -------------------------------------------------- */
+
