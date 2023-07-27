1ad56a6ac08ba578c3040aef242287a30bb30923
minomad <93kymins@gmail.com>
2023-07-27 15:51:49 +0900

    로컬스토리지 학습

--- client/main.js
+++ client/main.js
@@ -1,3 +1,24 @@
-console.log(a);
+import { deleteStorage, getNode, getStorage, setStorage } from './lib/index.js';
 
-const a = 10;
+const textField = getNode('#textField');
+const clearBtn = getNode('button');
+
+function handleTextField() {
+  const value = this.value;
+
+  setStorage('text', value);
+}
+
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+  });
+}
+
+function del() {
+  
+}
+
+textField.addEventListener('input', handleTextField);
+window.addEventListener('DOMContentLoaded', init);
+clearBtn.addEventListener('click', del)
